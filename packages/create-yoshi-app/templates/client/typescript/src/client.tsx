f10e9fb7ae0caddaa5c46e0a8d5bb9b04c2b4286
Vytenis Butkevicius <vytenisb@wix.com>
2019-06-25 14:27:06 +0300

    [client-generator] use @wix/wix-i18n-config for typescript generator

--- packages/create-yoshi-app/templates/client/typescript/src/client.tsx
+++ packages/create-yoshi-app/templates/client/typescript/src/client.tsx
@@ -1,25 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as axios from 'axios';
-import { I18nextProvider } from 'react-i18next';
+import { initI18n, I18nextProvider } from '@wix/wix-i18n-confin';
 import { wixAxiosConfig } from '@wix/wix-axios-config';
-import i18n from './i18n';
 import App from './components/App';
 import { create as createFedopsLogger } from '@wix/fedops-logger';
 
 const baseURL = window.__BASEURL__;
-const locale = window.__LOCALE__;
 
 wixAxiosConfig(axios, { baseURL });
 
 const fedopsLogger = createFedopsLogger('{%projectName%}');
-
+const i18n = initI18n({
+  locale: window.__LOCALE__,
+  asyncMessagesLoader: (locale: string) => import(`./locales/messages_${locale}.json`),
+});
 // Move the following `appLoaded()` call to the point where your app has fully loaded.
 // See https://github.com/wix-private/fed-infra/blob/master/fedops/fedops-logger/README.md
 fedopsLogger.appLoaded();
 
 ReactDOM.render(
-  <I18nextProvider i18n={i18n(locale)}>
+  <I18nextProvider i18n={i18n}>
     <App />
   </I18nextProvider>,
   document.getElementById('root'),
